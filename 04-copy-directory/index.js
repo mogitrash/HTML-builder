87d6285b66eb0cd03824ef6ba6bc098506316f62
Herman <hermanvasilkouskiy@gmail.com>
2024-01-20 19:36:50 +0300

    fix: delete exiting files before copying in 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,24 +1,43 @@
-const { dir } = require('console');
 const fs = require('fs');
 const path = require('path');
 
-fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
-  if (err) throw err;
-  console.log('Directory created successfully!');
-  copyDir(path.join(__dirname, 'files'), 'files-copy');
-});
+async function copyDir(srcDirPath, distDirName) {
+  try {
+    fs.mkdir(path.join(__dirname, distDirName), { recursive: true }, (err) => {
+      if (err) throw err;
+      console.log('Directory created successfully!');
+    });
 
-function copyDir(srcDirPath, distDirName) {
-  fs.promises.readdir(srcDirPath).then((data) =>
-    data.forEach((file) => {
-      fs.copyFile(
-        path.join(srcDirPath, file),
-        path.join(__dirname, distDirName, file),
-        (err) => {
-          if (err) throw err;
-          console.log(`File ${file} copied successfully!`);
-        },
-      );
-    }),
-  );
+    const distDirPath = path.join(__dirname, distDirName);
+    const filesCopy = await fs.promises.readdir(distDirPath);
+
+    for (const file of filesCopy) {
+      const distFilePath = path.join(__dirname, distDirName, file);
+
+      try {
+        await fs.promises.unlink(distFilePath);
+        console.log('File deleted!');
+      } catch {
+        console.log('DELETE_ERROR');
+      }
+    }
+
+    const files = await fs.promises.readdir(srcDirPath);
+
+    for (const file of files) {
+      const srcFilePath = path.join(srcDirPath, file);
+      const distFilePath = path.join(__dirname, distDirName, file);
+
+      try {
+        await fs.promises.copyFile(srcFilePath, distFilePath);
+        console.log('File copied!');
+      } catch {
+        console.log('COPY_ERROR');
+      }
+    }
+  } catch {
+    console.log('ERROR');
+  }
 }
+
+copyDir(path.join(__dirname, 'files'), 'files-copy');
